14bc0f4d2742a087df128a143c57708566a4120d
AfterStories <609449489@qq.com>
2018-04-13 09:30:16 +0800

    WhiteBoard
    WhiteBoard

---
+++ lib/combine.bat
@@ -0,0 +1,7 @@
+type CommandGroup.js >> WhiteBoard.js
+type CommandType.js >> WhiteBoard.js
+type Encoder.js >> WhiteBoard.js
+type Decoder.js >> WhiteBoard.js
+type CoderBuilder.js >> WhiteBoard.js
+type WebSocketHandler.js >> WhiteBoard.js
+type WhiteBoardHandler.js >> WhiteBoard.js
